ea39baf686a172b9a118193d921a1f7187d7e584
Aiden Deom <aiden.deom@gmail.com>
2014-12-11 17:12:42 -0500

    Added the new TDGs to the setup and teardowns

--- src/org/soen387/test/Teardown.java
+++ src/org/soen387/test/Teardown.java
@@ -6,6 +6,8 @@ import org.dsrg.soenea.service.tdg.UserTDG;
 import org.soen387.app.CheckersServlet;
 import org.soen387.domain.model.challenge.tdg.ChallengeTDG;
 import org.soen387.domain.model.checkerboard.tdg.CheckerBoardTDG;
+import org.soen387.domain.model.notification.challenge.tdg.ChallengeNotificationTDG;
+import org.soen387.domain.model.notification.game.tdg.GameNotificationTDG;
 import org.soen387.domain.model.player.tdg.PlayerTDG;
 
 public class Teardown {
@@ -17,6 +19,8 @@ public class Teardown {
 			PlayerTDG.dropTable();
 			ChallengeTDG.dropTable();
 			UserTDG.dropTable();
+			GameNotificationTDG.dropTable();
+			ChallengeNotificationTDG.dropTable();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
