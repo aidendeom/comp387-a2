d618c855b77b722c792add40276a747aa9cf7b65
Amit Malhotra <amit.malhotra@radialpoint.com>
2014-12-12 17:53:14 -0500

    Move began

---
+++ src/org/soen387/domain/command/MoveCommand.java
@@ -0,0 +1,11 @@
+package org.soen387.domain.command;
+
+import org.dsrg.soenea.domain.helper.Helper;
+
+public class MoveCommand extends CheckersCommand {
+
+	public MoveCommand(Helper myHelper) {
+		super(myHelper);
+	}
+
+}
