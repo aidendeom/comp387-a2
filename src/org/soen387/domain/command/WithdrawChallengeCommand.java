8a2b45119ff20483dbf9f20700dadf7915167f2d
Aiden Deom <aiden.deom@gmail.com>
2014-12-15 10:49:35 -0500

    Cleaned up WithdrawChallengeCommand

--- src/org/soen387/domain/command/WithdrawChallengeCommand.java
+++ src/org/soen387/domain/command/WithdrawChallengeCommand.java
@@ -1,6 +1,5 @@
 package org.soen387.domain.command;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.dsrg.soenea.domain.MapperException;
@@ -8,6 +7,7 @@ import org.dsrg.soenea.domain.command.CommandException;
 import org.dsrg.soenea.domain.command.impl.annotation.SetInRequestAttribute;
 import org.dsrg.soenea.domain.command.validator.source.IdentityBasedProducer;
 import org.dsrg.soenea.domain.command.validator.source.Source;
+import org.dsrg.soenea.domain.command.validator.source.impl.AttributeSource;
 import org.dsrg.soenea.domain.command.validator.source.impl.ParameterSource;
 import org.dsrg.soenea.domain.command.validator.source.impl.PermalinkSource;
 import org.dsrg.soenea.domain.helper.Helper;
@@ -18,12 +18,8 @@ import org.soen387.domain.command.exception.NeedToBeLoggedInException;
 import org.soen387.domain.model.challenge.ChallengeStatus;
 import org.soen387.domain.model.challenge.IChallenge;
 import org.soen387.domain.model.challenge.mapper.ChallengeInputMapper;
-import org.soen387.domain.model.checkerboard.CheckerBoardFactory;
 import org.soen387.domain.model.notification.challenge.ChallengeNotification;
-import org.soen387.domain.model.notification.challenge.ChallengeNotificationFactory;
-import org.soen387.domain.model.notification.challenge.ChallengeNotificationType;
 import org.soen387.domain.model.notification.challenge.mapper.ChallengeNotificationInputMapper;
-import org.soen387.domain.model.notification.challenge.mapper.ChallengeNotificationOutputMapper;
 
 public class WithdrawChallengeCommand extends CheckersCommand {
 
@@ -36,13 +32,8 @@ public class WithdrawChallengeCommand extends CheckersCommand {
 	@IdentityBasedProducer(mapper=ChallengeInputMapper.class)
 	public IChallenge challenge;
 	
-	@Source(sources=ParameterSource.class)
+	@Source(sources = {AttributeSource.class, ParameterSource.class})
 	public long version;
-	
-	//do we need to get status in url, we know we are withdrawing
-	@Source(sources=ParameterSource.class)
-	@IdentityBasedProducer(mapper=ChallengeStatus.ChallengeStatusProducer.class)
-	public ChallengeStatus status;
 	
 	@Override
 	public void process() throws CommandException {
@@ -70,16 +61,11 @@ public class WithdrawChallengeCommand extends CheckersCommand {
 			
 			//delete any unseen challengeIssued notifications
 			challengeNotifications = ChallengeNotificationInputMapper.findUnseen(challenge);
-			long notificationID;
-			ChallengeNotification notification;
-			if (challengeNotifications.size() > 0)
+			
+			for (int i = 0; i < challengeNotifications.size(); i++)
 			{
-				for (int i = 0; i < challengeNotifications.size(); i++)
-				{
-					notificationID = challengeNotifications.get(i).getId();
-					notification = ChallengeNotificationInputMapper.find(notificationID);
-					UoW.getCurrent().registerRemoved(notification);	
-				}
+			    ChallengeNotification n = challengeNotifications.get(i);
+				UoW.getCurrent().registerRemoved(n);	
 			}
 			
 			UoW.getCurrent().registerDirty(challenge);
